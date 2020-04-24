976eb83779b2352a753292852abeb0e1be1f78da
Hieu Nguyen <48639956+schwahue@users.noreply.github.com>
2020-04-24 12:00:16 +0800

    Practical 2 (#3)
    * Excercise 3
    
    * excercise 3

---
+++ student_codes/VideoJotter/routes/user.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+// User register URL using HTTP post => /user/register
+
+router.post('/register', (req, res) => {
+
+    let name = req.body.name;
+    let email = req.body.email;
+    let password1 = req.body.password;
+    let password2 = req.body.password2;
+    let errors = [];
+    let success_msg = email + ' is succesfully registered'; 
+
+    if (password1 == password2 && password1.length >= 4) {
+        res.render('user/login',{success_msg:success_msg})
+    }
+    else {
+        if (password1 != password2) {
+            errors.push({text: 'Passwords must match.'})
+        };
+        if (password1.length < 4){
+            errors.push({text: 'Password must be more than 4 characters.'})
+        };
+        res.render('user/register', {errors:errors, name:name, email:email});
+        errors = []
+    };
+
+
+});
+
+module.exports = router;
