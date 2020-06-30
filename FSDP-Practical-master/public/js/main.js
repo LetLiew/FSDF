8465e6d1ce993f670df0eb500f084d96b9831e5e
schwahue <48639956+schwahue@users.noreply.github.com>
2020-06-30 23:07:15 +0800

    Activity 1

--- FSDP-Practical-master/public/js/main.js
+++ FSDP-Practical-master/public/js/main.js
@@ -1,60 +1,81 @@
 function languageCheck() {
-    let checkBox = document.getElementsByName('language');
-    let errorMsg = document.getElementById('languageErr');
-    let submitButton = document.getElementById('butAddVideo');
+  let checkBox = document.getElementsByName("language");
+  let errorMsg = document.getElementById("languageErr");
+  let submitButton = document.getElementById("butAddVideo");
 
-    let checkStatus = 0;
+  let checkStatus = 0;
 
-    checkBox.forEach(function (item) {
-        if (item.checked == true) {
-            checkStatus += 1
-        }
-    });
-
-
-    if (checkStatus > 0) {
-        errorMsg.style.display = 'none';
-        submitButton.disabled = false;
+  checkBox.forEach(function (item) {
+    if (item.checked == true) {
+      checkStatus += 1;
     }
-    else {
-        errorMsg.style.display = 'block';
-        submitButton.disabled = true;
-    }
-};
+  });
+
+  if (checkStatus > 0) {
+    errorMsg.style.display = "none";
+    submitButton.disabled = false;
+  } else {
+    errorMsg.style.display = "block";
+    submitButton.disabled = true;
+  }
+}
 
 function getOMdbMovie() {
-    const title = document.getElementById('title').value;
-    const poster = document.getElementById('poster');
-    const omdbErr = document.getElementById('OMdbErr');
-    const posterURL = document.getElementById('posterURL');
-    const starring = document.getElementById('starring');
-    const story = document.getElementById('story');
-    const datepicker = document.getElementById('datepicker');
-    fetch('https://www.omdbapi.com/?t=' + title + '&apikey=60210937')
-        .then((res) => {
-            return res.json();
-        }).then((data) => {
-            if (data.Response === 'False') {
-                poster.src = '/img/no-image.jpg';
-                omdbErr.style.display = 'inline';
-            } else {
-                omdbErr.style.display = 'none';
-                poster.src = data.Poster;
-                starring.value = data.Actors;
-                posterURL.value = data.Poster; // hidden input field to submit
-                story.value = data.Plot;
-                datepicker.value = moment(new
-                    Date(data.Released)).format('DD/MM/YYYY');
-            }
-        }).catch(error => { omdbErr.innerHTML = error; })
-
+  const title = document.getElementById("title").value;
+  const poster = document.getElementById("poster");
+  const omdbErr = document.getElementById("OMdbErr");
+  const posterURL = document.getElementById("posterURL");
+  const starring = document.getElementById("starring");
+  const story = document.getElementById("story");
+  const datepicker = document.getElementById("datepicker");
+  fetch("https://www.omdbapi.com/?t=" + title + "&apikey=60210937")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      if (data.Response === "False") {
+        poster.src = "/img/no-image.jpg";
+        omdbErr.style.display = "inline";
+      } else {
+        omdbErr.style.display = "none";
+        poster.src = data.Poster;
+        starring.value = data.Actors;
+        posterURL.value = data.Poster; // hidden input field to submit
+        story.value = data.Plot;
+        datepicker.value = moment(new Date(data.Released)).format("DD/MM/YYYY");
+      }
+    })
+    .catch((error) => {
+      omdbErr.innerHTML = error;
+    });
 }
 
 function toUpperCase() {
-    let str = document.getElementById("title");
-    str.value = str.value.replace(
-        /\w\S*/g,
-        function(txt) {
-            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-        });
-  }
+  let str = document.getElementById("title");
+  str.value = str.value.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+  });
+}
+
+$("#posterUpload").on("change", function () {
+  let image = $("#posterUpload")[0].files[0];
+  let formdata = new FormData();
+  formdata.append("posterUpload", image);
+  $.ajax({
+    url: "/video/upload",
+    type: "POST",
+    data: formdata,
+    contentType: false,
+    processData: false,
+    success: (data) => {
+      $("#poster").attr("src", data.file);
+      $("#posterURL").attr("value", data.file); // sets posterURL hidden field
+      if (data.err) {
+        $("#posterErr").show();
+        $("#posterErr").text(data.err.message);
+      } else {
+        $("#posterErr").hide();
+      }
+    },
+  });
+});
--- FSDP-Practical-master/routes/video.js
+++ FSDP-Practical-master/routes/video.js
@@ -1,153 +1,191 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const moment = require('moment');
-const Video = require('../models/Video');
-const ensureAuthenticated = require('../helpers/auth');
-const alertMessage = require('../helpers/messenger');
+const moment = require("moment");
+const Video = require("../models/Video");
+const ensureAuthenticated = require("../helpers/auth");
+const alertMessage = require("../helpers/messenger");
+
+// Required for file upload
+const fs = require("fs");
+const upload = require("../helpers/imageUpload");
 
 // Adds new video jot from /video/addVideo
-router.post('/addVideoJot',ensureAuthenticated, (req, res) => {
-    let title = req.body.title;
-    let story = req.body.story.slice(0, 1999);
-    let dateRelease = moment(req.body.dateRelease, 'DD/MM/YYYY');
-    let language = req.body.language.toString();
-    let subtitles = req.body.subtitles === undefined ? '' : req.body.subtitles.toString();
-    let classification = req.body.classification;
-    let userId = req.user.id;
-    let starring = req.body.starring;
-    let posterURL = req.body.posterURL;
+router.post("/addVideoJot", ensureAuthenticated, (req, res) => {
+  let title = req.body.title;
+  let story = req.body.story.slice(0, 1999);
+  let dateRelease = moment(req.body.dateRelease, "DD/MM/YYYY");
+  let language = req.body.language.toString();
+  let subtitles =
+    req.body.subtitles === undefined ? "" : req.body.subtitles.toString();
+  let classification = req.body.classification;
+  let userId = req.user.id;
+  let starring = req.body.starring;
+  let posterURL = req.body.posterURL;
 
-    // Multi-value components return array of strings or undefined
-    Video.create({
-        title,
-        story,
-        classification,
-        language,
-        subtitles,
-        dateRelease,
-        userId,
-        starring,
-        posterURL,
-    }).then((video) => {
-        res.redirect('/video/listVideos');
+  // Multi-value components return array of strings or undefined
+  Video.create({
+    title,
+    story,
+    classification,
+    language,
+    subtitles,
+    dateRelease,
+    userId,
+    starring,
+    posterURL,
+  })
+    .then((video) => {
+      res.redirect("/video/listVideos");
     })
-        .catch(err => console.log(err))
+    .catch((err) => console.log(err));
 });
 
 // List videos belonging to current logged in user
-router.get('/listVideos',ensureAuthenticated, (req, res) => {
-
-    Video.findAll({
-        where: {
-            userId: req.user.id
-        },
-        order: [
-            ['title', 'ASC']
-        ],
-        raw: true
+router.get("/listVideos", ensureAuthenticated, (req, res) => {
+  Video.findAll({
+    where: {
+      userId: req.user.id,
+    },
+    order: [["title", "ASC"]],
+    raw: true,
+  })
+    .then((videos) => {
+      // pass object to listVideos.handlebar
+      res.render("video/listVideos", {
+        videos: videos,
+      });
     })
-        .then((videos) => {
-            // pass object to listVideos.handlebar
-            res.render('video/listVideos', {
-                videos: videos
-            });
-        })
-        .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
-router.get('/showAddVideo',ensureAuthenticated, (req, res) => {
-    res.render('video/addVideo');
+router.get("/showAddVideo", ensureAuthenticated, (req, res) => {
+  res.render("video/addVideo");
 });
 
 // Shows edit video page
-router.get('/edit/:id',ensureAuthenticated, (req, res) => {
-    Video.findOne({
-        where: {
-            id: req.params.id
-        }
-    }).then((video) => {
-        checkOptions(video);
-        if (req.user.id === video.userId){
-        res.render('video/editVideo', {         // call views/video/editVideo.handlebar to render the edit video page
-            video // passes video object to handlebar
-        });}
-    }).catch(err => console.log(err)); // To catch no video ID
+router.get("/edit/:id", ensureAuthenticated, (req, res) => {
+  Video.findOne({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((video) => {
+      checkOptions(video);
+      if (req.user.id === video.userId) {
+        res.render("video/editVideo", {
+          // call views/video/editVideo.handlebar to render the edit video page
+          video, // passes video object to handlebar
+        });
+      }
+    })
+    .catch((err) => console.log(err)); // To catch no video ID
 });
-
-
 
 // Creates variables with ‘check’ to put a tick in the appropriate checkbox
 function checkOptions(video) {
-    video.chineseLang = (video.language.search('Chinese') >= 0) ? 'checked' : '';
-    video.englishLang = (video.language.search('English') >= 0) ? 'checked' : '';
-    video.malayLang = (video.language.search('Malay') >= 0) ? 'checked' : '';
-    video.tamilLang = (video.language.search('Tamil') >= 0) ? 'checked' : '';
+  video.chineseLang = video.language.search("Chinese") >= 0 ? "checked" : "";
+  video.englishLang = video.language.search("English") >= 0 ? "checked" : "";
+  video.malayLang = video.language.search("Malay") >= 0 ? "checked" : "";
+  video.tamilLang = video.language.search("Tamil") >= 0 ? "checked" : "";
 
-    //subtitles
-    video.chineseSub = (video.subtitles.search('Chinese') >= 0) ? 'checked' : '';
-    video.englishSub = (video.subtitles.search('English') >= 0) ? 'checked' : '';
-    video.malaySub = (video.subtitles.search('Malay') >= 0) ? 'checked' : '';
-    video.tamileSub = (video.subtitles.search('Tamil') >= 0) ? 'checked' : '';
+  //subtitles
+  video.chineseSub = video.subtitles.search("Chinese") >= 0 ? "checked" : "";
+  video.englishSub = video.subtitles.search("English") >= 0 ? "checked" : "";
+  video.malaySub = video.subtitles.search("Malay") >= 0 ? "checked" : "";
+  video.tamileSub = video.subtitles.search("Tamil") >= 0 ? "checked" : "";
 }
 
 //save Edited Video
-router.put('/edit/saveEditedVideo/:id',ensureAuthenticated, (req, res) => {
-    let title = req.body.title;
-    let story = req.body.story.slice(0, 1999);
-    let dateRelease = moment(req.body.dateRelease, "DD/MM/YYYY");
-    let language = req.body.language.toString();
-    let subtitles = req.body.subtitles === undefined ? '' :
-        req.body.subtitles.toString();
+router.put("/edit/saveEditedVideo/:id", ensureAuthenticated, (req, res) => {
+  let title = req.body.title;
+  let story = req.body.story.slice(0, 1999);
+  let dateRelease = moment(req.body.dateRelease, "DD/MM/YYYY");
+  let language = req.body.language.toString();
+  let subtitles =
+    req.body.subtitles === undefined ? "" : req.body.subtitles.toString();
 
-    let classification = req.body.classification;
-    let starring = req.body.starring;
-    let posterURL = req.body.posterURL;
+  let classification = req.body.classification;
+  let starring = req.body.starring;
+  let posterURL = req.body.posterURL;
 
-    Video.update({
-        title,
-        story,
-        classification,
-        language,
-        subtitles,
-        dateRelease,
-        starring,
-        posterURL,
-    }, {
-        where: {
-            id: req.params.id
-        }
-    }).then((video) => {
-        res.redirect('/video/listVideos');
-    }).catch(err => console.log(err));
+  console.log(posterURL);
+
+  Video.update(
+    {
+      title,
+      story,
+      classification,
+      language,
+      subtitles,
+      dateRelease,
+      starring,
+      posterURL,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
+    .then((video) => {
+      res.redirect("/video/listVideos");
+    })
+    .catch((err) => console.log(err));
 });
 
-router.get('/delete/:id',ensureAuthenticated, (req,res) =>{
-    let videoId = req.params.id;
-    let userId = req.user.id;
+router.get("/delete/:id", ensureAuthenticated, (req, res) => {
+  let videoId = req.params.id;
+  let userId = req.user.id;
 
-    Video.findOne({
-        where:{
-            id:videoId,
-            userID: userId,
-        }
-    }).then((video)=>{
-        let title = video.title
+  Video.findOne({
+    where: {
+      id: videoId,
+      userID: userId,
+    },
+  })
+    .then((video) => {
+      let title = video.title;
 
-        if(video == null){
-            res.redirect('/logout');
-        }
-        else{
-            Video.destroy({
-                where: {
-                id: videoId
-                }
-                }).then((video) =>{
-                    alertMessage(res, 'success','Video ' + title + ' is successfully deleted', 'fas fa-sign-in-alt', true)
-                    res.redirect('/video/listVideos');
-                })
-        };
-    }).catch(err => console.log(err));
+      if (video == null) {
+        res.redirect("/logout");
+      } else {
+        Video.destroy({
+          where: {
+            id: videoId,
+          },
+        }).then((video) => {
+          alertMessage(
+            res,
+            "success",
+            "Video " + title + " is successfully deleted",
+            "fas fa-sign-in-alt",
+            true
+          );
+          res.redirect("/video/listVideos");
+        });
+      }
+    })
+    .catch((err) => console.log(err));
 });
 
+// Upload poster
+router.post("/upload", ensureAuthenticated, (req, res) => {
+  // Creates user id directory for upload if not exist
+  if (!fs.existsSync("./public/uploads/" + req.user.id)) {
+    fs.mkdirSync("./public/uploads/" + req.user.id);
+  }
+
+  upload(req, res, (err) => {
+    if (err) {
+      res.json({ file: "/img/no-image.jpg", err: err });
+    } else {
+      if (req.file === undefined) {
+        res.json({ file: "/img/no-image.jpg", err: err });
+      } else {
+        res.json({ file: `/uploads/${req.user.id}/${req.file.filename}` });
+      }
+    }
+  });
+});
 
 module.exports = router;
